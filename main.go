f4b2dc613d524d4be21213229d87ec685a5176f9
Skyler Kimura <skyler8@hawaii.edu>
2025-06-06 12:32:48 -1000

    extracted repl logic into repl file

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
-
 func main() {
-	for {
-		fmt.Print("Pokedex > ")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan();
-		userInput := scanner.Text()
-		cleanedInput := cleanInput(userInput)
-		fmt.Println("Your command was:", cleanedInput[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"strings"
+	"os"
+	"bufio"
+	"fmt"
 )
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan();
+		input := cleanInput(scanner.Text())
+
+		if len(input) == 0 {
+			continue
+		}
+
+		commandName := input[0]
+
+		fmt.Println("Your command was:", commandName)
+	}
 }
